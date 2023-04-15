6b1fd691d95485cb4f30845857c0ee7076d678f1
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-04-15 02:34:08 -0700

    Update head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,19 @@
 import '@/styles/globals.css'
 import Layout from '@/components/layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Kappa</title>
+
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Component {...pageProps} />
     </Layout>
   )
